5c0a323679793ccb8ced77314efeaaf7b3048d56
Robert Novotny <novotnr0@gmail.com>
2021-03-09 00:30:20 +0100

    Pridany viewholder vratane layoutu v XML

--- app/src/main/java/com/github/novotnyr/drink/DrinkListAdapter.kt
+++ app/src/main/java/com/github/novotnyr/drink/DrinkListAdapter.kt
@@ -1,6 +1,8 @@
 package com.github.novotnyr.drink
 
+import android.view.View
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 
 object DrinkDiff : DiffUtil.ItemCallback<Drink>() {
     override fun areItemsTheSame(oldItem: Drink, newItem: Drink): Boolean {
@@ -11,3 +13,5 @@ object DrinkDiff : DiffUtil.ItemCallback<Drink>() {
         return oldItem == newItem
     }
 }
+
+class DrinkViewHolder(layout: View) : RecyclerView.ViewHolder(layout)
