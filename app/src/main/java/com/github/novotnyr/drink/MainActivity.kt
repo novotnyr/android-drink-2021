a7f83193650b57fed8af7e16ea477b123288cbab
Robert Novotny <novotnr0@gmail.com>
2021-03-10 01:46:46 +0100

    Pridany RecyclerView, ktory je prepojeny s adapterom

--- app/src/main/java/com/github/novotnyr/drink/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/drink/MainActivity.kt
@@ -2,10 +2,22 @@ package com.github.novotnyr.drink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drinkRecyclerView: RecyclerView
+
+    private val adapter = DrinkListAdapter().apply {
+        submitList(listOf(Drink()))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drinkRecyclerView = findViewById(R.id.drinkRecyclerView)
+        drinkRecyclerView.layoutManager = GridLayoutManager(this, 3)
+        drinkRecyclerView.adapter = adapter
     }
 }
