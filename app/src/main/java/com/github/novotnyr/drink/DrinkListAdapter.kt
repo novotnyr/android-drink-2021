e2b0e06170eccb39b515c469f68076ebf4d57e35
Robert Novotny <novotnr0@gmail.com>
2021-03-10 01:40:47 +0100

    Pridany adapter pre RecyclerView

--- app/src/main/java/com/github/novotnyr/drink/DrinkListAdapter.kt
+++ app/src/main/java/com/github/novotnyr/drink/DrinkListAdapter.kt
@@ -1,7 +1,10 @@
 package com.github.novotnyr.drink
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 object DrinkDiff : DiffUtil.ItemCallback<Drink>() {
@@ -15,3 +18,16 @@ object DrinkDiff : DiffUtil.ItemCallback<Drink>() {
 }
 
 class DrinkViewHolder(layout: View) : RecyclerView.ViewHolder(layout)
+
+class DrinkListAdapter : ListAdapter<Drink, DrinkViewHolder>(DrinkDiff) {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DrinkViewHolder {
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.drink, parent, false)
+        return DrinkViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: DrinkViewHolder, position: Int) {
+        // nothing
+    }
+}
