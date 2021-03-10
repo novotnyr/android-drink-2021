c5aa8a1929129d7c180e0c88ff48c62d64027854
Robert Novotny <novotnr0@gmail.com>
2021-03-10 13:52:13 +0100

    Po kliknuti na FAB sa zobrazi Snackbar

--- app/src/main/java/com/github/novotnyr/drink/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/drink/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var drinkRecyclerView: RecyclerView
@@ -22,5 +23,11 @@ class MainActivity : AppCompatActivity() {
         drinkRecyclerView.adapter = adapter
     }
 
-    fun onFloatingActionButtonClick(view: View) {}
+    fun onFloatingActionButtonClick(floatingActionButton: View) {
+        Snackbar.make(floatingActionButton, "Na zdravie!", Snackbar.LENGTH_LONG)
+                .apply {
+                    anchorView = floatingActionButton
+                }
+                .show()
+    }
 }
